cec8f7a852ca985933f87d2e81da9b537dc0dd10
ademiralcojr <ademiralcojr@gmail.com>
2020-08-03 11:43:53 -0300

    Finla commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './App.css';
+import FirstComponent from './components/learning-examples/FirstComponent';
+import SecondComponent from './components/learning-examples/SecondComponent';
+import ThirdComponent from './components/learning-examples/ThirdComponent';
+import Counter from './components/Counter/Counter';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Counter/>
+      </div>
+    );
+  }
+}
+
+class LearningComponents extends Component {
+  render() {
+    return (
+      <div className="LearnigComponents">
+        My Hello World.
+        <FirstComponent />
+        <SecondComponent />
+        <ThirdComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/learning-examples/ThirdComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ThirdComponent() {
+    return (
+      <div className="ThirdComponent">
+        Third Component.
+      </div>
+    );
+  }
+
+export default ThirdComponent;
